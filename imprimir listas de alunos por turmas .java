44b3d7c19c02fe1ea3160f5cad6e0150f83d3cba
yonder22 <40334860+yonder22@users.noreply.github.com>
2018-06-17 20:13:08 +0100

    Add files via upload

---
+++ imprimir listas de alunos por turmas .java
@@ -0,0 +1,85 @@
+public class imprimir listas de alunos por turmas  {
+
+	private int Ano;
+
+	private string Turno;
+
+	private string Turma;
+
+	public void imprimir listas() {
+
+	}
+
+}
+public class Calcular notas {
+
+	private int 1ªFrequencia;
+
+	private int 2ªFrequencia;
+
+
+	public void Somar valores () {
+
+        public void Calcular notas () {
+
+        public void Apresentar resultado () {
+
+	}
+
+}
+public class lancar nota por turma  {
+
+
+	private string Aluno;
+
+	private string Disciplina;
+
+	public void imprimir notas de avalicoes () {
+        
+        public void lancar nota() {
+
+	}
+
+}
+
+public class registrar entrada saida  {
+
+
+	private string Nome;
+
+	private int Codigo;
+
+        private string Descricao;
+
+	public void Fazer Leitura () {
+        
+
+	}
+
+}
+
+
+public class lancar nota por turma  {
+
+
+	private int Nome;
+
+	private Descricao docente;
+
+	public void Registrar entrada/saída () {
+        
+        public void imprimir lista() {
+        
+        public void Lançar nota() {
+
+        public void Imprimir nota de avaliacoes() {
+
+	}
+
+}
+
+}
+
+}
+
+
